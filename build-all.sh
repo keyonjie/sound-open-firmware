56fe9d00e27be721121145fc483a3c02829c79f7
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2017-11-27 05:37:30 +0800

    library: Add support for building audio components as a library
    Add a library build target so that audio processing components can
    be built as a stand alone shared or static library for either the host
    or DSP targets.
    
    This enables running components in a host based simulator, debugger and
    test harness.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- build-all.sh
+++ build-all.sh
@@ -9,17 +9,34 @@ pwd=`pwd`
 
 rm -fr src/arch/xtensa/*.ri
 
+# build library for host platform architecture
+./configure --with-arch=host --enable-library=yes --host=x86_64-unknown-linux-gnu --prefix=$pwd/../host-root/
+make
+make install
+
 # Build for Baytrail
-make clean
 ./configure --with-arch=xtensa --with-platform=baytrail --with-root-dir=$pwd/../xtensa-root/xtensa-byt-elf --host=xtensa-byt-elf
+make clean
 make
 make bin
 
-# Build for Cherrytrail
+# build library for xtensa byt architectures
+./configure --with-arch=xtensa --with-platform=baytrail --with-root-dir=$pwd/../xtensa-root/xtensa-byt-elf --host=xtensa-byt-elf --enable-library=yes --prefix=$pwd/../xtensa-root/xtensa-byt-elf
 make clean
+make
+make install
+
+# Build for Cherrytrail
 ./configure --with-arch=xtensa --with-platform=cherrytrail --with-root-dir=$pwd/../xtensa-root/xtensa-byt-elf --host=xtensa-byt-elf
+make clean
 make
 make bin
+
+# build library for xtensa cht architectures
+./configure --with-arch=xtensa --with-platform=baytrail --with-root-dir=$pwd/../xtensa-root/xtensa-byt-elf --host=xtensa-byt-elf --enable-library=yes --prefix=$pwd/../xtensa-root/xtensa-byt-elf
+make clean
+make
+make install
 
 # list all the images
 ls -l src/arch/xtensa/*.ri
